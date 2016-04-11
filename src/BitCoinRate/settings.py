8e44fbe971877f5b3fd3b89b454810cc34c7f311
Hieu Le <hieutle@Hieus-MBP.home>
2016-04-11 00:57:41 -0400

    removing .pyc files

--- src/BitCoinRate/settings.py
+++ src/BitCoinRate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'currentrate',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'BitCoinRate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
